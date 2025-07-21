20046a1b57aff812a432830b27176d619f2af5d9
Gabriel Neves <gabrielnevesoliveiraquintao@gmail.com>
2025-07-21 14:51:44 -0300

    App Final

---
+++ scripts/script.js
@@ -0,0 +1,160 @@
+// Declaração de constantes
+const menu      = document.getElementById('menu')
+const cartBtn   = document.getElementById('cart-btn')
+const cartModal = document.getElementById('cart-modal')
+const cartItemsContainer = document.getElementById('cart-items')
+const cartTotal = document.getElementById('cart-total')
+const checkoutBtn = document.getElementById('checkout-btn')
+const cartModalBtn = document.getElementById('cart-modal-btn')
+const closeModalBtn = document.getElementById('close-modal-btn')
+const cartCounter  = document.getElementById('cart-counter')
+const addressInput = document.getElementById('address')
+const addressWarn  = document.getElementById('address-warn')
+
+// Array do carrinho
+let cart = [];
+
+// Abrir modal do carrinho
+cartBtn.addEventListener('click', function() {
+    updateCartModal();
+    cartModal.style.display = 'flex';
+});
+
+// Fechar modal quando clicar fora
+cartModal.addEventListener('click', function(event) {
+    if(event.target === cartModal) {
+        cartModal.style.display = 'none'
+    }
+});
+
+// Fechar modal quando clicar no botão "fechar"
+closeModalBtn.addEventListener('click', function() {
+    cartModal.style.display = 'none'
+});
+
+// Agregando itens selecionados no carrinho
+menu.addEventListener('click', function(event) {
+    let parentButton = event.target.closest('.add-to-cart-btn')
+    if(parentButton) {
+        const name  = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+        addToCart(name, price)
+    }
+});
+
+// Adicionando no carrinho
+function addToCart(name, price) {
+    const existingItem = cart.find(item => item.name === name)
+    // Se item já existir aumenta quantidade
+    if(existingItem) {
+        existingItem.quantity += 1;
+    } else {
+        cart.push({
+            name, price, quantity: 1,
+        });
+    }
+    updateCartModal();
+}
+
+// Atualizando o carrinho
+function updateCartModal() {
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement('div');
+        cartItemElement.classList.add('flex', 'justify-between', 'mb-4', 'flex-col')
+        cartItemElement.innerHTML = `
+            <div class="flex items-center justify-between">
+                <div>
+                    <p class="font-medium">${item.name}<p>
+                    <p>Quantidade: ${item.quantity}</p>
+                    <p class="font-medium mt-2">${item.price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>
+                </div>
+                <button style="color:red" class="remove-from-cart-btn" data-name="${item.name}">Remover</button>
+            </div>
+        `;
+        total += item.price * item.quantity;
+        cartItemsContainer.appendChild(cartItemElement)
+    });
+
+    cartTotal.textContent = total.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    });
+
+    cartCounter.innerHTML = cart.length;
+}
+
+// Removendo do carrinho
+cartItemsContainer.addEventListener('click', function(event) {
+    if(event.target.classList.contains('remove-from-cart-btn')) {
+        const name = event.target.getAttribute('data-name')
+        removeItemCart(name)
+    }
+})
+
+function removeItemCart(name) {
+    const index = cart.findIndex(item => item.name === name);
+    if(index !== -1) {
+        const item = cart[index];
+        if(item.quantity > 1) {
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+        cart.splice(index, 1);
+        updateCartModal();
+    }
+}
+
+// Endereço
+addressInput.addEventListener('input', function(event) {
+    let inputValue = event.target.value;
+    if(inputValue !== "") {
+        addressInput.classList.remove('border-red-500')
+        addressWarn.classList.add('hidden')
+    }
+});
+
+// Checkout
+checkoutBtn.addEventListener('click', function() {
+    const isOpen = checkOpen();
+    if(!isOpen) {
+        alert('Loja fechada no momento!')
+        return;
+    }
+
+    if(cart.lenght === 0) return;
+    if(addressInput.value === "") {
+        addressWarn.classList.remove('hidden');
+        addressInput.classList.add('border-red-500');
+        return;
+    }
+
+    // Enviando pro zap
+    const cartItems = cart.map((item) => {
+        `${item.name} Quantidade: (${item.quantity}) Preço: ${item.price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} | `
+    }).join("");
+    const message = encodeURIComponent(cartItems);
+    const phone   = '31994525631';
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, '_blank')
+});
+
+// Verificando o horário de funcionamento
+function checkOpen() {
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22;
+}
+
+// Mudando status da loja
+const spanItem = document.getElementById('date-span')
+const isOpen = checkOpen()
+if(isOpen) {
+    spanItem.classList.remove('bg-red-500');
+    spanItem.classList.add('bg-green-600')
+} else {
+    spanItem.classList.add('bg-red-500');
+    spanItem.classList.remove('bg-green-600')
+}
